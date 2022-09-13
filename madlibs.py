61b2e512a91088675dc18908b436293a6fed6eb4
pratikshagadhe23 <113328762+pratikshagadhe23@users.noreply.github.com>
2022-09-12 22:07:55 -0700

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,8 +1,9 @@
 flower= input("Enter a flower: ")
 color =input("Enter a color: ")
 celebrity = input("Enter a celebrity: ")
-
+number = input("Enter a number : ")
 
 print("Roses are " +flower)
 print("Violets are " + color)
 print("I love " + celebrity)
+print("Lucky" + number)
