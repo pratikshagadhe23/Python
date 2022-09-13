24e60371adacd5a0ed405c985551d0488b9acedd
pratikshagadhe23 <pratikshagadhe94@gmail.com>
2022-09-12 22:09:43 -0700

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,8 +1,10 @@
 flower= input("Enter a flower: ")
 color =input("Enter a color: ")
 celebrity = input("Enter a celebrity: ")
+name = input("Enter a name: ")
 
 
 print("Roses are " +flower)
 print("Violets are " + color)
 print("I love " + celebrity)
+print("My name is "+ name) 
