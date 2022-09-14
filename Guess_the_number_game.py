09e4435ea96f5ce2bb5dca3918403a3790a8d569
pratikshagadhe23 <pratikshagadhe94@gmail.com>
2022-09-14 11:36:20 -0700

    Create Guess_the_number_game.py

---
+++ Guess_the_number_game.py
@@ -0,0 +1,14 @@
+import random
+
+number = random.randrange(1,50)
+guess = int(input("Guess a number between 1 and 50 : "))
+
+while guess!=number:
+    if guess> number:
+        print("Guess a lower number. Try again")
+        guess = int(input("\nGuess a number between 1 and 50 : "))
+    else:
+        print("Guess a higher number. Try again")
+        guess = int(input("\nGuess a number between 1 and 50 : "))
+        
+print("Congratulations! You guessed the right number.")
